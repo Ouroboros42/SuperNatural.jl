97d33fb6a4ba35d8a1b1cb62ee8da35449bf9650
Lucas Gathercole <lucas.gathercole@gmail.com>
2026-03-04 17:53:36 +0100

    Test variant units

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,4 +8,9 @@ using Test
     @test natural(u"ft") ≈ natural(u"ns") rtol = 0.05
     @test natdim(10u"m") == -1
     @test natdim(u"c") == 0
+    @test natural(u"q^-2") ≈ 137 rtol = 0.05
+
+    weirdunits = oneunit_system(u"kg", 2u"m/s", u"N")
+
+    @test natural(4u"m", weirdunits) ≈ 1u"kg"
 end
